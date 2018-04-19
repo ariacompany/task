cff9ba524aa27c2a4d99dd88d14510282d883ab6
Fendi <ariacompany89@gmail.com>
2018-04-19 22:40:58 +0700

    include javascript lib, jquery,jquery ui, bootstrap

--- task/resources/views/TaskHome.blade.php
+++ task/resources/views/TaskHome.blade.php
@@ -1,12 +1,19 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="{{asset('css/custom.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/custom.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/jquery-ui.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+
+    <script type="text/javascript" href="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script type="text/javascript" href="{{asset('js/jquery-ui.min.js')}}"></script>
+    <script type="text/javascript" href="{{asset('js/popper.min.js')}}"></script>
+    <script type="text/javascript" href="{{asset('js/bootstrap.min.js')}}"></script>
 </head>
 <body>
 <select id="drpDownAdventure">
 
 </select>
-<button class="font-Oldenburg">New Adventure</button>
-<button class="font-Oldenburg">New Task</button>
+<button type="button" class="font-Oldenburg btn btn-primary">New Adventure</button>
+<button type="button" class="font-Oldenburg btn btn-primary">New Task</button>
 </body>
 </html>
